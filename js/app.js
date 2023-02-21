7dcefa895a25b31b07a6ab7f3c16f1f2f4eadd8b
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-21 15:39:05 +0100

    FIX html inentation

--- js/app.js
+++ js/app.js
@@ -1,45 +1,45 @@
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-          slideImages: [
-          {
-              image: './img/img-1.jpeg',
-              title: "Guard ò mar quant'è bell",
-              text: 'Mare del folfo di Napoli in tempresta',
-          },    
-          {
-              image: './img/img-2.jpeg',
-              title: "Pinocchio, atto primo.",
-              text: 'Tavole del patio da spazzare',
-          },
-          {
-              image: './img/img-3.jpeg',
-              title: "AUGH!.",
-              text: 'Scaccia sogni di età pre coloniale',
-          },
-          {
-              image: './img/img-4.jpeg',
-              title: "Mbare! L'etna si sta svegliando!",
-              text: 'Altopiano vulcanico siculo in prospettiva',
-          },
-          {
-              image: './img/img-5.jpg',
-              title: "Chi me l'ha fatto fare",
-              text: 'Cercando di studiare js',
-          },
-          {
-              image: './img/img-6.jpg',
-              title: "3,2,1, Buon anno!",
-              text: 'Festeggiamenti di capodanno in Wyoming',
-          },
-      ],
+    data() {
+        return {
+            slideImages: [
+            {
+                image: './img/img-1.jpeg',
+                title: "Guard ò mar quant'è bell",
+                text: 'Mare del folfo di Napoli in tempresta',
+            },    
+            {
+                image: './img/img-2.jpeg',
+                title: "Pinocchio, atto primo.",
+                text: 'Tavole del patio da spazzare',
+            },
+            {
+                image: './img/img-3.jpeg',
+                title: "AUGH!.",
+                text: 'Scaccia sogni di età pre coloniale',
+            },
+            {
+                image: './img/img-4.jpeg',
+                title: "Mbare! L'etna si sta svegliando!",
+                text: 'Altopiano vulcanico siculo in prospettiva',
+            },
+            {
+                image: './img/img-5.jpg',
+                title: "Chi me l'ha fatto fare",
+                text: 'Cercando di studiare js',
+            },
+            {
+                image: './img/img-6.jpg',
+                title: "3,2,1, Buon anno!",
+                text: 'Festeggiamenti di capodanno in Wyoming',
+            },
+            ],
 
-      currentIndex: 0,
+            currentIndex: 0,
       
-    }
-  },
+        }
+    },
 
   methods:{
 
